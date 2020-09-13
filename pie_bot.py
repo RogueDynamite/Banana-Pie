254cd104fb69f6f4ec7e96974c032b1014e6c0d0
Nicholas De Leon <ndianaty@hotmail.com>
2020-09-13 13:25:17 -0700

    Fix a bug in bot message and add in a help command.

--- pie_bot.py
+++ pie_bot.py
@@ -9,7 +9,9 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
+    if message.content == 'bp!help':
+        await message.channel.send("Type bp!pie for some banana pie! You can also visit my website for some banana pie! https://banana-pie.herokuapp.com/")
     if message.content == 'bp!pie':
-        await message.channel.send('Here\'s your pie {message.author.mention}!\nhttps://img.buzzfeed.com/video-api-prod/assets/f78b2d95fc904dfb908e21862da36534/YT_THUMB.jpg')
+        await message.channel.send(f'Here\'s your pie {message.author.mention}!\nhttps://img.buzzfeed.com/video-api-prod/assets/f78b2d95fc904dfb908e21862da36534/YT_THUMB.jpg')
 
 client.run(os.environ['BOT_TOKEN'])
